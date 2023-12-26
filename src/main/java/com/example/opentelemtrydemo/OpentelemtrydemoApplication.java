74aaf6c4c21f5974c19fdaee85d9d5bd1b67d534
anhar <anhar9617@gmail.com>
2023-12-27 01:38:03 +0300

    opentelemetry demo

---
+++ src/main/java/com/example/opentelemtrydemo/OpentelemtrydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.opentelemtrydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentelemtrydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpentelemtrydemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/opentelemtrydemo/OpentelemtrydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opentelemtrydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpentelemtrydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
